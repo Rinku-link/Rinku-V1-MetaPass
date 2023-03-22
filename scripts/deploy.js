9bc9f9c99d5dcf822c04c796dc442814ee337ff7
Jaylon <jaylon@metaboomstudio.com>
2023-03-22 21:08:21 +0800

    add Joy first batch, changed SoulboundBadge name

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -13,15 +13,6 @@ async function main() {
 
   // print the address of the deployed contract
   console.log("NFT Contract Address:", deployedNFTContract.address);
-
-  const soulBoundNftContract = await ethers.getContractFactory("SoulBoundBadge");
-
-  // here we deploy the contract
-  const deployedsoulBoundNftContract = await soulBoundNftContract.deploy();
-
-  // wait for the contract to deploy
-  await deployedsoulBoundNftContract.deployed();
-  console.log("Soul Bound NFT Contract Address:", deployedsoulBoundNftContract.address);
 }
 
 // Call the main function and catch if there is any error
